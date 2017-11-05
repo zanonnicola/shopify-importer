93706f53f156fa90dd5c9c5ba25f5af31a0658da
Nicola Zanon <nicola.zanon86@gmail.com>
2017-11-05 18:01:22 +0000

    Add API limit control

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ require("dotenv").config();
 const csv = require("csvtojson");
 const path = require("path");
 const axios = require("axios");
+const delay = require("delay");
 
 const ROOT = __dirname;
 const filePath = path.normalize(path.join(ROOT, "csv/customers.csv"));
 const customers = [];
-
-console.log(process.env.SHOP_NAME, process.env.API_KEY, process.env.PASSWORD);
-
-// const shopify = new Shopify({
-//   shopName: process.env.SHOP_NAME,
-//   apiKey: process.env.API_KEY,
-//   password: process.env.PASSWORD,
-//   autoLimit: true,
-//   bucketSize: true
-// });
 
 csv({ checkColumn: true, workerNum: 3 })
   .fromFile(filePath)
@@ -26,9 +17,11 @@ csv({ checkColumn: true, workerNum: 3 })
     if (error) {
       throw new Error(`Something went wrong ${error}`);
     }
-    console.log(customers.length);
+    console.log(`# of Customers: ${customers.length}`);
     createCustomer(customers);
   });
+
+let limit = 40;
 
 function createCustomer(arr) {
   arr.forEach(person => {
@@ -59,42 +52,54 @@ function createCustomer(arr) {
     };
     const json = JSON.stringify(data);
 
-    axios
-      .post(
-        `https://${process.env.SHOP_NAME}.myshopify.com/admin/customers.json`,
-        json,
-        {
-          headers: { "Content-Type": "application/json; charset=utf-8" },
-          withCredentials: true,
-          auth: {
-            username: process.env.API_KEY,
-            password: process.env.PASSWORD
-          }
-        }
-      )
-      .then(function(response) {
-        console.log(
-          response.headers["x-shopify-shop-api-call-limit"],
-          response.status
-        );
-      })
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
+    if (limit > 3) {
+      postData(json);
+    } else {
+      delay(2000).then(() => {
+        postData(json);
       });
+    }
   });
+}
+
+function postData(json) {
+  axios
+    .post(
+      `https://${process.env.SHOP_NAME}.myshopify.com/admin/customers.json`,
+      json,
+      {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        withCredentials: true,
+        auth: {
+          username: process.env.API_KEY,
+          password: process.env.PASSWORD
+        }
+      }
+    )
+    .then(function(response) {
+      console.log(
+        response.headers["x-shopify-shop-api-call-limit"],
+        response.status
+      );
+      limit -= response.headers["x-shopify-shop-api-call-limit"].split("/")[0];
+      console.log(`Limit: ${limit}`);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 }
