8f05e6eeaa0376a34eb0442d8f7d1e00a6cdcc69
Nicola Zanon <nicola.zanon86@gmail.com>
2017-11-06 21:21:34 +0000

    Product handler

---
+++ entities/products.js
@@ -0,0 +1,31 @@
+module.exports = product => {
+  const data = {
+    product: {
+      title: product.name,
+      body_html: product.description,
+      vendor: product.brand,
+      product_type: "machine",
+      tags: "",
+      metafields_global_title_tag: product.meta_title,
+      metafields_global_description_tag: product.meta_description,
+      variants: [
+        {
+          price: product.price,
+          sku: "",
+          inventory_quantity: product.quantity,
+          inventory_management: "shopify",
+          weight: 0.0,
+          weight_unit: "lb",
+          requires_shipping: true
+        }
+      ],
+      images: [
+        {
+          src:
+            "https://shop.finishingtouchesgroup.com/image/data/2017%20new/li-pigment%20banner.jpg"
+        }
+      ]
+    }
+  };
+  return data;
+};
--- index.js
+++ index.js
@@ -4,26 +4,28 @@ const path = require("path");
 const delay = require("delay");
 
 const customerSchema = require("./entities/customer");
+const productSchema = require("./entities/products");
 const postDataToShopify = require("./network");
 
 const ROOT = __dirname;
 const filePath = path.normalize(path.join(ROOT, "csv/customers.csv"));
+const filePathProducts = path.normalize(path.join(ROOT, "csv/products.csv"));
 const customers = [];
+const products = [];
+let limit = 40;
 
 csv({ checkColumn: true, workerNum: 3 })
-  .fromFile(filePath)
+  .fromFile(filePathProducts)
   .on("json", jsonObj => {
-    customers.push(jsonObj);
+    products.push(jsonObj);
   })
   .on("done", error => {
     if (error) {
       throw new Error(`Something went wrong ${error}`);
     }
-    console.log(`# of Customers: ${customers.length}`);
-    createCustomer(customers);
+    console.log(`# of products: ${products.length}`);
+    createProduct(products);
   });
-
-let limit = 40;
 
 function createCustomer(arr) {
   arr.forEach(person => {
@@ -35,6 +37,21 @@ function createCustomer(arr) {
     } else {
       delay(2000).then(() => {
         postDataToShopify(json, "admin/customers.json", limit);
+      });
+    }
+  });
+}
+
+function createProduct(arr) {
+  arr.forEach(product => {
+    const data = productSchema(product);
+    const json = JSON.stringify(data);
+
+    if (limit > 3) {
+      postDataToShopify(json, "/admin/products.json", limit);
+    } else {
+      delay(2000).then(() => {
+        postDataToShopify(json, "/admin/products.json", limit);
       });
     }
   });
